61f99f70ffc47a9c8c736ee99ac9127ed64b6443
jae yeop <woco11@naver.com>
2024-08-09 14:03:04 +0900

    데이터 삭제

--- src/test/java/com/example/test/TestApplicationTests.java
+++ src/test/java/com/example/test/TestApplicationTests.java
@@ -1,6 +1,8 @@
 package com.example.test;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 import com.example.test.Entity.Question;
 import com.example.test.Repositroy.QuestionRepository;
@@ -9,18 +11,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @SpringBootTest
 class TestApplicationTests {
 
-    @Autowired
+    @Autowired //의존성 주입
     private QuestionRepository questionRepository;
 
     @Test
     void testJpa() {
-        Question q = this.questionRepository.findBySubjectAndContent("sbb가 무엇인가요?","sbb에 대해서 알고 싶습니다.");
-        assertEquals(1,q.getId());
+        assertEquals(2,questionRepository.count());//행갯수 리턴
+        Optional<Question> oq = questionRepository.findById(1);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+        this.questionRepository.delete(q);
+        assertEquals(1,this.questionRepository.count());
     }
 
 }
