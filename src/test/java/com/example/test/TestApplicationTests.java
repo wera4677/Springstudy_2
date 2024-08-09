b8607e7a42b868e411589e33e168eb29aa733aa0
jae yeop <woco11@naver.com>
2024-08-09 14:38:16 +0900

    조회2

--- src/test/java/com/example/test/TestApplicationTests.java
+++ src/test/java/com/example/test/TestApplicationTests.java
@@ -8,6 +8,7 @@ import com.example.test.Entity.Answer;
 import com.example.test.Entity.Question;
 import com.example.test.Repositroy.AnswerRepository;
 import com.example.test.Repositroy.QuestionRepository;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -25,12 +26,17 @@ class TestApplicationTests {
     @Autowired
     private AnswerRepository answerRepository;
 
+    @Transactional
     @Test
     void testJpa() {
-        Optional<Answer> oa = this.answerRepository.findById(1);
-        assertTrue(oa.isPresent());
-        Answer a = oa.get();
-        assertEquals(2,a.getQuestion().getId());
+        Optional<Question> oq = this.questionRepository.findById(2);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+
+        List<Answer> answerList = q.getAnswersList();
+
+        assertEquals(1,answerList.size());
+        assertEquals("네 자동으로 생성됩니다.",answerList.get(0).getContent());
     }
 
 }
