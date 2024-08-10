1d3478e864f7db8d77daf8a910dc0fec51cfa10a
jae yeop <woco11@naver.com>
2024-08-10 22:00:45 +0900

    list

--- src/main/java/com/example/sbb/question/QuestionController.java
+++ src/main/java/com/example/sbb/question/QuestionController.java
@@ -1,15 +1,22 @@
 package com.example.sbb.question;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
 
+@RequiredArgsConstructor
 @Controller
 public class QuestionController {
+    private final QuestionSevice questionSevice ;
 
     @GetMapping("/question/list")
 
-    public String list(){
+    public String list(Model model){
+        List<Question> questionList =this.questionSevice.getList();
+        model.addAttribute("questionList", questionList);
         return "question_list";
     }
 }
