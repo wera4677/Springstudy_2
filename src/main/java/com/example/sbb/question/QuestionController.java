323f25611167d197ef5af5f456891da9abd2a9b4
jae yeop <woco11@naver.com>
2024-08-13 20:46:49 +0900

    질문등록

--- src/main/java/com/example/sbb/question/QuestionController.java
+++ src/main/java/com/example/sbb/question/QuestionController.java
@@ -28,4 +28,8 @@ public class QuestionController {
         model.addAttribute("question", question);
         return "question_detail";
     }
+    @GetMapping("/create")
+    public String questionCreate(){
+        return "question_form";
+    }
 }
