067aea4338e353d532511226a8e028ada3a91882
jae yeop <woco11@naver.com>
2024-08-19 22:06:15 +0900

    signup form

--- src/main/java/com/example/sbb/User/UserController.java
+++ src/main/java/com/example/sbb/User/UserController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor //생성자 주입
@@ -15,12 +16,18 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/signup")
+    public String signup(UserCreateForm userCreateForm){
+        return "signup_form";
+    }
+
+
+    @PostMapping("/signup")
     public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult){
         if(bindingResult.hasErrors()){
             return "signup_form";
         }
 
-        if (!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())){
+        if (!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())){//비밀번호 확인
             bindingResult.rejectValue("password2","passwordInCorrect","2개의 비밀번호가 일치하지 않습니다.");
             return "signup_form";
         }
