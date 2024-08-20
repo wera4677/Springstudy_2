6fb9454e40b4d2261e6b2097e84613bc65894db8
jae yeop <woco11@naver.com>
2024-08-20 23:04:35 +0900

    select username

--- src/main/java/com/example/sbb/User/UserRepository.java
+++ src/main/java/com/example/sbb/User/UserRepository.java
@@ -2,6 +2,9 @@ package com.example.sbb.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 //리포지터리 생성
 public interface UserRepository extends JpaRepository<SiteUser,Long> {
+    Optional<SiteUser> findByUsername(String username);
 }
