a8298c1e7a560af09b65b40fc7f432423d12fcf9
jae yeop <woco11@naver.com>
2024-09-04 17:50:44 +0900

    답변 삭제 기능 추가

--- src/main/java/com/example/sbb/answer/AnswerController.java
+++ src/main/java/com/example/sbb/answer/AnswerController.java
@@ -65,4 +65,14 @@ public class AnswerController {
         this.answerService.modify(answer,answerForm.getContent());
         return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
     }
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/delete/{id}")
+    public String answerDelete(@PathVariable("id") Integer id,Principal principal) {
+        Answer answer = this.answerService.getAnswer(id);
+        if(!answer.getAuthor().getUsername().equals(principal.getName())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"삭제 권한이 없습니다.");
+        }
+        this.answerService.delete(answer);
+        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+    }
 }
--- src/main/java/com/example/sbb/answer/AnswerService.java
+++ src/main/java/com/example/sbb/answer/AnswerService.java
@@ -37,4 +37,7 @@ public class AnswerService {
         answer.setModifyDate(LocalDateTime.now());
         this.answerRepository.save(answer);
     }
+    public void delete(Answer answer) {
+        this.answerRepository.delete(answer);
+    }
 }
