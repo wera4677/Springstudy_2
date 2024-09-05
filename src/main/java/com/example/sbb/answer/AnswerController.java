17b6edc10b1f2ad7a9a268579071737e222d7338
jae yeop <woco11@naver.com>
2024-09-05 16:06:10 +0900

    답변 추천 기능2

--- src/main/java/com/example/sbb/answer/AnswerController.java
+++ src/main/java/com/example/sbb/answer/AnswerController.java
@@ -65,6 +65,7 @@ public class AnswerController {
         this.answerService.modify(answer,answerForm.getContent());
         return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
     }
+
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/delete/{id}")
     public String answerDelete(@PathVariable("id") Integer id,Principal principal) {
@@ -73,6 +74,15 @@ public class AnswerController {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"삭제 권한이 없습니다.");
         }
         this.answerService.delete(answer);
+        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/vote/{id}")
+    public String answerVote(@PathVariable("id") Integer id,Principal principal) {
+        Answer answer = this.answerService.getAnswer(id);
+        SiteUser siteUser = this.userService.getUser(principal.getName());
+        this.answerService.vote(answer,siteUser);
         return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
     }
 }
